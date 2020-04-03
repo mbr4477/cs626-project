71441ad1e028dc31d1d23c9bca196822b114c03b
Peclet <adeniji_david_omotayo@yahoo.com>
2020-04-03 11:24:50 -0400

    // First Mapper Initial

--- src/main/kotlin/com/datascience/project/Playlist_Mapper.kt
+++ src/main/kotlin/com/datascience/project/Playlist_Mapper.kt
@@ -20,12 +20,13 @@ class Playlist_Mapper: Mapper<LongWritable, Text, NullWritable, Text>() {
         val lineStr = line.toString()
         val sortedLine = lineStr
             .split(',')
-            .sorted()
-            .joinToString(",")
+
+
+
         // write the sorted pair
         // this line writes to the map reduce context to provide
         // the intermediate outputs for the reducer
         // output the sorted key and 1
-        context.write(Text(sortedLine), IntWritable(1))
+        context.write(NullWritable.get(), Text(sortedLine[2]))
     }
 }
